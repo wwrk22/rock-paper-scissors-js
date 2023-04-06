4e04cc116e290fa3f0651fd22db6fd2fb5ec7812
Won Rhim <wwrk22@gmail.com>
2023-04-06 12:49:20 -0400

    Add full game playing logic

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const moves = ["Rock", "Paper", "Scissors"];
+const human = "human"
+const computer = "computer";
 
 function getComputerChoice() {
   const index = Math.floor(Math.random() * 3);
@@ -18,6 +20,7 @@ function formatChoice(choice) {
 }
 
 function determineWinner(playerChoice, computerChoice) {
+  console.log(`Human played ${playerChoice} and Computer played ${computerChoice}.`);
   if (playerChoice === computerChoice) return null;
   if (playerChoice === moves[0]) return playRock(computerChoice);
   if (playerChoice === moves[1]) return playPaper(computerChoice);
@@ -26,19 +29,46 @@ function determineWinner(playerChoice, computerChoice) {
 }
 
 function playRock(computerChoice) {
-  if (computerChoice === moves[1]) return "computer";
-  if (computerChoice === moves[2]) return "human";
+  if (computerChoice === moves[1]) return computer;
+  if (computerChoice === moves[2]) return human;
   else return undefined;
 }
 
 function playPaper(computerChoice) {
-  if (computerChoice === moves[0]) return "human";
-  if (computerChoice === moves[2]) return "computer";
+  if (computerChoice === moves[0]) return human;
+  if (computerChoice === moves[2]) return computer;
   else return undefined;
 }
 
 function playScissors(computerChoice) {
-  if (computerChoice === moves[0]) return "computer";
-  if (computerChoice === moves[1]) return "human";
+  if (computerChoice === moves[0]) return computer;
+  if (computerChoice === moves[1]) return human;
   else return undefined;
+}
+
+function game() {
+  humanScore = 0;
+  computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const playerChoice = prompt("Make a move. Choices are \"rock\", \"paper\", and \"scissors\".");
+    const winner = playRound(playerChoice, getComputerChoice());
+    updateScores(winner);
+  }
+
+  console.log(`Final result: Human - ${humanScore}, Computer - ${computerScore}`);
+}
+
+function updateScores(winner) {
+  if (winner === null) {
+    console.log("Round resulted in a tie. No updates to the scores will be made.");
+  } else if (winner === human) {
+    console.log("Human wins the round.");
+    humanScore++;
+  } else if (winner === computer) {
+    console.log("Computer wins the round.");
+    computerScore++;
+  } else {
+    console.log("Something went terribly wrong. No updates to the scores will be made.");
+  }
 }
